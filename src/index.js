0fa7d8f130988a4a8620b1a5a1d931f58d501457
Figby222 <figby222@outlook.com>
2024-03-15 22:37:20 -0700

    Return promise from getWeather()

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 function getWeather(location) {
-    fetch(`https://api.weatherapi.com/v1/current.json?key=786cfcfa7077405f90982207241303&q=${location}`, {mode: "cors"})
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(response) {
-        processData(response).then(result => console.log(result));
-    })
-    .catch(function(err) {
-        console.error(err);
-    })
+        return fetch(`https://api.weatherapi.com/v1/current.json?key=786cfcfa7077405f90982207241303&q=${location}`, {mode: "cors"})
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(response) {
+            return response;
+        })
+        .catch(function(err) {
+            console.error(err);
+        })
 }
 
 function processData(json) {
